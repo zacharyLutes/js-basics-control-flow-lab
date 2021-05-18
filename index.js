8b3684d4f0b9c8429d5f42f58d5add59413aa4aa
forkingits1123 <lblack11235813@gmail.com>
2021-05-18 08:23:28 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance <= 400) {
+    const free = true;
+    return 'This one is on me!';
+  } else if (distance >= 2500) {
+    const nope = true;
+    return 'No can do.'
+  } 
+  if (distance > 2000) {
+    return 'I will gladly take your thirty bucks.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city){
+  if (city === "NYC") {
+    return 'Ok, sounds good.';
+  } else if (city !== "NYC") {
+    return 'No go.';
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if (tip === 'generous') {
+    return 'Thank you so much.';
+  } else if (tip === 'not as generous') {
+    return 'Thank you.';
+  } else {
+    return 'Bye.';
+  }
 }
